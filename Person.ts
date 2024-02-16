c0325a681fd1ab9fbece673050ecf6ec7af3b8cc
OlegStore <159705200+OlegStore@users.noreply.github.com>
2024-02-16 22:38:35 +0100

    Update Person.ts

--- Person.ts
+++ Person.ts
@@ -2,9 +2,11 @@
 class Person {
     // Properties
     private name: string;
-    private age: number;
-
-    // Constructor
+    private age
+    private profesion: string;
+ 
+ 
+ // Constructor
     constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
